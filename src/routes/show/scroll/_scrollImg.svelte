3874312862f0044641c433ae0fa98642e66827a9
Maxime Dupont <maxime.dupont@jamespot.com>
2022-07-26 06:08:22 +0200

    try fix scroll img effect

--- src/routes/show/scroll/_scrollImg.svelte
+++ src/routes/show/scroll/_scrollImg.svelte
@@ -33,6 +33,8 @@
 		const top = i?.getBoundingClientRect()?.top || 10000;
 		const bottom = i?.getBoundingClientRect()?.bottom || 10000;
 
+		console.log(i, top, bottom, (top + bottom) / 2, floor((top + bottom) / 2))
+
 		j = floor((top + bottom) / 2);
 	}
 
