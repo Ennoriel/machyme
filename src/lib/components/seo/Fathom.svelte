944388a37abd9cd70b1b29f241671dcd916db73c
Maxime Dupont <m.dupont103@gmail.com>
2023-09-21 19:04:00 +0200

    tidy up fathom config

--- src/lib/components/seo/Fathom.svelte
+++ src/lib/components/seo/Fathom.svelte
@@ -3,20 +3,18 @@
 	import { page } from '$app/stores';
 	import { PUBLIC_FATHOM_ID, PUBLIC_FATHOM_URL } from '$env/static/public';
 	import { isProd } from '$lib/utils/store';
-	import * as Fathom from 'fathom-client';
+	import { load, trackPageview } from 'fathom-client';
 
 	let init = false;
 
 	$: if ($isProd && browser) {
 		if (!init) {
-			console.log("init")
-			Fathom.load(PUBLIC_FATHOM_ID, {
+			load(PUBLIC_FATHOM_ID, {
 				url: PUBLIC_FATHOM_URL
 			});
 			init = true;
 		} else if (/* tracking page change */ $page.url.pathname) {
-			console.log("change")
-			Fathom.trackPageview();
+			trackPageview();
 		}
 	}
 </script>
