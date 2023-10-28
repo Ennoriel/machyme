22ffce9feadcfc611122fa792279b1b5c9e2703f
Maxime Dupont <m.dupont103@gmail.com>
2023-10-28 06:46:38 +0200

    remove fathom

--- src/lib/components/layout/ContactLink.svelte
+++ src/lib/components/layout/ContactLink.svelte
@@ -1,7 +1,3 @@
-<script>
-	import { trackGoal } from 'fathom-client';
-</script>
-
-<a href="mailto:m.dupont103@gmail.com" on:click={() => trackGoal(`UL95OT2G`, 100)}>
+<a href="mailto:m.dupont103@gmail.com">
 	<slot>Email</slot>
 </a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script>
-	import Fathom from '$lib/components/seo/Fathom.svelte';
 	import { isProd } from '$lib/utils/store';
 
 	export let data;
 
 	$: $isProd = data.isProd;
 </script>
-
-<Fathom />
 
 <slot />
